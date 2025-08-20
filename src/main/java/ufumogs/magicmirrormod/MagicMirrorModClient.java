9f00b95483739019a63715c48802b012be6e14c5
Ufuk Koza <ufuk.koza@student.manchester.ac.uk>
2025-08-20 10:01:21 +0300

    Initial commit

---
+++ src/main/java/ufumogs/magicmirrormod/MagicMirrorModClient.java
@@ -0,0 +1,10 @@
+package ufumogs.magicmirrormod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicMirrorModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/ufumogs/magicmirrormod/MagicMirrorModDataGenerator.java
@@ -0,0 +1,11 @@
+package ufumogs.magicmirrormod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MagicMirrorModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
